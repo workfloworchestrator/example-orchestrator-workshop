3b06a11417eef4c3ceb6a1f87b95b90ec2ea8c54
Brian Eschen <beschen@beschen-mbp.dhcp.lbl.gov>
2023-04-03 14:34:40 -0700

    circuit product type

---
+++ products/product_types/circuit.py
@@ -0,0 +1,30 @@
+from orchestrator.domain.base import SubscriptionModel
+from orchestrator.types import SubscriptionLifecycle
+
+from product_blocks.circuit import (
+    Circuit,
+    CircuitInactive,
+    CircuitProvisioning,
+)
+
+# In here, we define the values expected for a product block at each phase of the of the Subscription Lifecycle
+# All resource types used by a product block need to be explicitly called out here and assigned
+# expected types
+
+
+class CircuitInactive(SubscriptionModel, is_base=True):
+    # Equipment state is planned
+    speed: str
+    ckt: CircuitInactive
+    
+    
+class CircuitProvisioning(CircuitInactive, lifecycle=[SubscriptionLifecycle.PROVISIONING]):
+    # Equipment state is Commissioning
+    speed: str
+    ckt: CircuitProvisioning
+    
+    
+class Circuit(CircuitProvisioning, lifecycle=[SubscriptionLifecycle.ACTIVE]):
+    # Equipment state is Provisioned
+    speed: str
+    ckt: Circuit
