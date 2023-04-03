625d74d508473bdea671d92f2534981592bf17d2
Nehemya McCarter-Ribakoff <nemi@es.net>
2023-04-03 15:48:02 -0700

    fix: node domain model types

--- products/product_blocks/node.py
+++ products/product_blocks/node.py
@@ -15,7 +15,7 @@ class NodeBlockInactive(ProductBlockModel, product_block_name="Node"):
     # when first created we expect all resources to be none
     node_id: Optional[int] = None
     node_name: Optional[str] = None
-    ipv4_ipam_id: Optional[UUID] = None
+    ipv4_ipam_id: Optional[str] = None
     ipv6_ipam_id: Optional[str] = None
 
 
@@ -25,7 +25,7 @@ class NodeBlockProvisioning(NodeBlockInactive, lifecycle=[SubscriptionLifecycle.
     # In the Provisioning state, there will be an ESDB node, Device Group, and NSO service assigned
     node_id: int
     node_name: str
-    ipv4_ipam_id: UUID
+    ipv4_ipam_id: str
     ipv6_ipam_id: str
 
 
@@ -34,7 +34,6 @@ class NodeBlock(NodeBlockProvisioning, lifecycle=[SubscriptionLifecycle.ACTIVE])
 
     # In the active state, there will be an ESDB node, Device Group, and NSO service assigned
     node_id: int
-    esdb_node_uuid: UUID
     node_name: str
-    ipv4_ipam_id: UUID
+    ipv4_ipam_id: str
     ipv6_ipam_id: str
