9778ef30d30b6147d11a904fab5e98f343819d53
Nehemya McCarter-Ribakoff <nemi@es.net>
2023-04-03 14:44:07 -0700

    chore: fix formatting

--- workflows/shared.py
+++ workflows/shared.py
@@ -1,23 +1,14 @@
-import structlog
-from orchestrator.forms import FormPage, ReadOnlyField
-from orchestrator.forms.validators import Choice, OrganisationId
+from typing import Callable, Optional
+
 from orchestrator.targets import Target
-from orchestrator.types import FormGenerator, SubscriptionLifecycle
-from orchestrator.types import InputForm, InputStepFunc, SubscriptionLifecycle, UUIDstr
+from orchestrator.types import InputStepFunc, SubscriptionLifecycle
 from orchestrator.workflow import StepList, Workflow, done, init, make_workflow
+from orchestrator.workflows.steps import resync, set_status
 from orchestrator.workflows.utils import wrap_create_initial_input_form
-from orchestrator.workflows.steps import resync, set_status, store_process_subscription
 
-from orchestrator.workflow import StepList, begin
-from orchestrator.workflows.steps import set_status, store_process_subscription
-from pydantic import validator
-from typing import Any, Callable, Dict, Optional
-
-
-
-logger = structlog.get_logger(__name__)
 
 CUSTOMER_UUID = "b727dd2c-55f3-4d19-8452-a32f15b00123"
+
 
 def create_workflow(
     description: str,
@@ -35,10 +26,14 @@ def create_workflow(
             do_something
             >> do_something_else
     """
-    create_initial_input_form_generator = wrap_create_initial_input_form(initial_input_form)
+    create_initial_input_form_generator = wrap_create_initial_input_form(
+        initial_input_form
+    )
 
     def _create_workflow(f: Callable[[], StepList]) -> Workflow:
         steplist = init >> f() >> set_status(status) >> resync >> done
-        return make_workflow(f, description, create_initial_input_form_generator, Target.CREATE, steplist)
+        return make_workflow(
+            f, description, create_initial_input_form_generator, Target.CREATE, steplist
+        )
 
     return _create_workflow
