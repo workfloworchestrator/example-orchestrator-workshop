746c10a8a7ac859fc7c83e891b7ffa19f3bf506e
Derek Howard <derek@howderek.com>
2023-04-04 14:50:23 -0700

    fix: data load

--- etc/netbox/entrypoint.sh
+++ etc/netbox/entrypoint.sh
@@ -3,6 +3,8 @@
 if [ -f "/etc/netbox/data.json" ]; then
     echo "Loading data from  /etc/netbox/data.json"
     /opt/netbox/netbox/manage.py loaddata -v 3 /etc/netbox/data.json
+else
+    echo "no data to load"
 fi
 
-/opt/netbox/docker-entrypoint.sh /opt/netbox/launch-netbox.sh
+/opt/netbox/launch-netbox.sh
