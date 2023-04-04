1cdfb246a78dfb09db7ccc28ebc6f023b8aea460
Nehemya McCarter-Ribakoff <nemi@es.net>
2023-04-04 13:52:31 -0700

    feat: update netbox creds

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
-from netbox import NetBox # type: ignore
+from netbox import NetBox  # type: ignore
 
-netbox = NetBox(host='127.0.0.1', port=32768, use_ssl=False, auth_token='token')
+netbox = NetBox(
+    host="netlab-rover7.es.net",
+    port=8000,
+    use_ssl=False,
+    auth_token="e744057d755255a31818bf74df2350c26eeabe54",
+)
