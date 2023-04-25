983eee9efb5e504f159aa598280f16cdafe560a2
chriscummings <chriscummings@es.net>
2023-04-25 14:35:38 -0500

    chore: cleanup and comments for circuit

--- workflows/circuit/create_circuit.py
+++ workflows/circuit/create_circuit.py
@@ -1,29 +1,30 @@
 """Workflow to initially create a circuit between two nodes."""
-from typing import Any, List, Generator
 from ipaddress import IPv6Interface
-
+from typing import Any, Generator, List
 
 import structlog
-from orchestrator.forms import FormPage, ReadOnlyField
-from orchestrator.forms.validators import Choice, Accept, LongText
-from orchestrator.targets import Target
-from orchestrator.types import FormGenerator, State, SubscriptionLifecycle, UUIDstr
-from orchestrator.workflow import StepList, begin, step, inputstep
-from orchestrator.workflows.steps import set_status, store_process_subscription
 from orchestrator.config.assignee import Assignee
-from products.product_types.circuit import CircuitInactive, CircuitProvisioning
-from products.product_types.node import Node
+from orchestrator.forms import FormPage, ReadOnlyField
+from orchestrator.forms.validators import Accept, Choice, LongText
 from orchestrator.services.subscriptions import (
     retrieve_subscription_by_subscription_instance_value,
 )
+from orchestrator.targets import Target
+from orchestrator.types import FormGenerator, State, SubscriptionLifecycle, UUIDstr
+from orchestrator.workflow import StepList, begin, inputstep, step
+from orchestrator.workflows.steps import set_status, store_process_subscription
+
+from products.product_types.circuit import CircuitInactive, CircuitProvisioning
+from products.product_types.node import Node
 from utils import netbox
 
 from ..shared import CUSTOMER_UUID, create_workflow
 
-
 logger = structlog.get_logger(__name__)
 
+# The ID of the Subnet Block we will use for assigning IPs to circuits
 CIRCUIT_PREFIX_IPAM_ID = 3
+ISIS_AREA_ID = "49.0001.0123.4567.890a.0001.00"
 
 
 def get_valid_cables_list() -> List[Any]:
@@ -31,9 +32,7 @@ def get_valid_cables_list() -> List[Any]:
     Connects to netbox and returns a list of valid netbox circuits/cables.
     """
     logger.info("Connecting to Netbox to get list of available circuits/cables")
-    valid_circuits = list(
-        netbox.dcim.cables.filter(status="planned")
-    )  # TODO: Add error handling
+    valid_circuits = list(netbox.dcim.cables.filter(status="planned"))
     logger.info(f"Found {len(valid_circuits)} circuits/cables in Netbox")
     return valid_circuits
 
@@ -52,24 +51,23 @@ def format_circuits(circuit_list: List[Any]) -> Generator:
         else:
             raise ValueError("Could not pull full details of circuit")
 
-    # yield next(zip(circuit_list, pretty_circuits))
-    # return zip(circuit_list, pretty_circuits) #TODO turn into a generator?
-
-
-def get_next_ip() -> Any:
-    pass
-
 
 def initial_input_form_generator(product_name: str) -> FormGenerator:
     """
     Generates the Circuit Form to display to the user.
     """
     logger.info("Generating initial input form for Circuit")
+    # TODO: Add validator to make sure that the nodes this circuit depends on already exist.
+
+    # First, get the data we need to present a list of circuits to a user
     valid_circuits = get_valid_cables_list()
     pretty_circuits = list(format_circuits(valid_circuits))
-    choices = [node[1] for node in pretty_circuits]
+
+    # Then format the circuits into a choice list:
+    choices = [circuit[1] for circuit in pretty_circuits]
     CircuitEnum = Choice("CircuitEnum", zip(choices, choices))
 
+    # Next, construct the form to display to the user:
     class CreateCircuitForm(FormPage):
         """FormPage for Creating a Circuit"""
 
@@ -78,18 +76,26 @@ def initial_input_form_generator(product_name: str) -> FormGenerator:
 
             title = product_name
 
-        select_node_choice: CircuitEnum  # type: ignore
+        select_circuit_choice: CircuitEnum  # type: ignore
 
+    # Now we present the form to the user
+    logger.info("Presenting CreateCircuitForm to user")
     user_input = yield CreateCircuitForm
-    logger.info(f"User selected circuit/cable {user_input.select_node_choice}")
+
+    # Once we get the form selecton back from the user we send that circuit ID
+    # to the state object for use in generating the subscription
+    logger.info(f"User selected circuit/cable {user_input.select_circuit_choice}")
     circuit_data = next(
-        node[0] for node in pretty_circuits if user_input.select_node_choice == node[1]
+        circuit[0]
+        for circuit in pretty_circuits
+        if user_input.select_circuit_choice == circuit[1]
     )
-    logger.info(f"circuit data is: {circuit_data.__dict__}")
+
+    logger.info("Done with CreateCircuitForm")
 
     return {
         "circuit_id": circuit_data.id,
-        "pretty_circuit": user_input.select_node_choice,
+        "pretty_circuit": user_input.select_circuit_choice,
     }
 
 
@@ -98,40 +104,46 @@ def construct_circuit_model(
     product: UUIDstr,
     state: State,
 ) -> State:
+    """
+    Construct the initial domain model for circuit
+    """
     logger.info("Building initial circuit model")
 
+    # First, we instantiate an empty subscription for a circuit subscription:
     subscription = CircuitInactive.from_product_id(
         product_id=product,
         customer_id=CUSTOMER_UUID,
         status=SubscriptionLifecycle.INITIAL,
     )
 
+    # Next, we add the circuit details to the subscription
     logger.info("Adding Base Circuit Model fields to Subscription")
     subscription.ckt.circuit_id = state.get("circuit_id")
     subscription.ckt.under_maintenance = True
 
-    logger.info("getting circuit details from netbox")
-    netbox_circuit = netbox.dcim.cables.get(subscription.ckt.circuit_id)
-    netbox_circuit.full_details()  # TODO: Raise error
-
+    # Then, we pull in the full details of this circuit from netbox for use later on:
     logger.info(
-        f"Grabbing Device IDs for circuit {subscription.ckt.circuit_id} from netbox"
+        f"Getting circuit details for Circuit {subscription.ckt.circuit_id} from NetBox"
+    )
+    netbox_circuit = netbox.dcim.cables.get(subscription.ckt.circuit_id)
+    netbox_circuit.full_details()
+    logger.info(
+        f"Grabbing Device IDs for circuit {subscription.ckt.circuit_id} from NetBox"
     )
 
     node_a_netbox_id = str(netbox_circuit.a_terminations[0].device.id)
     node_b_netbox_id = str(netbox_circuit.b_terminations[0].device.id)
 
     logger.info("Linking existing node subscriptions")
-    # Get the existing node subscriptions from the DB
-    # TODO: Add validator to Form Generator to make sure that these already exist.
-    # TODO: Raise an error here if they don't exist
+    # Now, we get the existing node subscriptions from the DB:
+
     node_a_subscription = retrieve_subscription_by_subscription_instance_value(
         resource_type="node_id", value=node_a_netbox_id
     )
     node_b_subscription = retrieve_subscription_by_subscription_instance_value(
         resource_type="node_id", value=node_b_netbox_id
     )
-    # Link the existing node subscriptions from the DB to the circuit subscription
+    # Link the existing node subscriptions from the DB to the circuit subscription:
     subscription.ckt.members[0].port.node = Node.from_subscription(
         node_a_subscription.subscription_id
     ).node
@@ -139,23 +151,20 @@ def construct_circuit_model(
         node_b_subscription.subscription_id
     ).node
 
+    # Here, we construct the port product block portions of the domain model:
     subscription.ckt.members[0].port.port_name = netbox_circuit.a_terminations[
         0
     ].display
     subscription.ckt.members[1].port.port_name = netbox_circuit.b_terminations[
         0
     ].display
-
-    # Set the port details that we have already
     subscription.ckt.members[0].port.port_id = netbox_circuit.a_terminations[0].id
-    subscription.ckt.members[
-        0
-    ].port.port_description = f"Circuit Connection to {subscription.ckt.members[1].port.node.node_name} port {subscription.ckt.members[1].port.port_name}"
+    port_a_description = f"Circuit Connection to {subscription.ckt.members[1].port.node.node_name} port {subscription.ckt.members[1].port.port_name}"
+    subscription.ckt.members[0].port.port_description = port_a_description
 
     subscription.ckt.members[1].port.port_id = netbox_circuit.b_terminations[0].id
-    subscription.ckt.members[
-        1
-    ].port.port_description = f"Circuit Connection to {subscription.ckt.members[0].port.node.node_name} port {subscription.ckt.members[0].port.port_name}"
+    port_b_description = f"Circuit Connection to {subscription.ckt.members[0].port.node.node_name} port {subscription.ckt.members[0].port.port_name}"
+    subscription.ckt.members[1].port.port_description = port_b_description
 
     return {
         "subscription": subscription,
@@ -164,24 +173,46 @@ def construct_circuit_model(
 
 
 @step("Reserve IPs in Netbox")
-def reserve_ips_in_ipam(
-    subscription: CircuitInactive,
-) -> State:
+def reserve_ips_in_ipam(subscription: CircuitInactive, state: State) -> State:
+    """
+    Reserves the IPs for this circuit in Netbox and adds them to the domain model
+    """
     logger.info("Reserving IPs in NetBox")
     circuit_block_prefix = netbox.ipam.prefixes.get(CIRCUIT_PREFIX_IPAM_ID)
+    # First, reserve a /64 pool for future use.
     current_circuit_prefix_64 = circuit_block_prefix.available_prefixes.create(
-        {"prefix_length": 64, "description": "foo", "is_pool": "on"}
-    )  # TODO: Set Description for prefix
+        {
+            "prefix_length": 64,
+            "description": f"{state.get('pretty_circuit')} Parent /64",
+            "is_pool": "on",
+        }
+    )
+    # Next, reserve a /127 point-to-point subnet for the circuit (from the above /64).
     current_circuit_prefix_127 = current_circuit_prefix_64.available_prefixes.create(
-        {"prefix_length": 127, "description": "foo", "is_pool": "on"}
-    )  # TODO: Set Description for prefix
+        {
+            "prefix_length": 127,
+            "description": f"{state.get('pretty_circuit')} Point-to-Point",
+        }
+    )
+    # Now, create the NetBox IP Address entries for the devices on each side of the link:
     a_side_ip = current_circuit_prefix_127.available_ips.create(
-        {"description": "foo"}
-    )  # TODO: Set Description for IP, add to device and interface, then set status to planning
+        {
+            "description": subscription.ckt.members[0].port.port_description,
+            "assigned_object_type": "dcim.interface",
+            "assigned_object_id": subscription.ckt.members[0].port.port_id,
+            "status": "active",
+        }
+    )
     b_side_ip = current_circuit_prefix_127.available_ips.create(
-        {"description": "foo"}
-    )  # TODO: Set Description for IP, add to device and interface, then set status to planning
+        {
+            "description": subscription.ckt.members[1].port.port_description,
+            "assigned_object_type": "dcim.interface",
+            "assigned_object_id": subscription.ckt.members[1].port.port_id,
+            "status": "active",
+        }
+    )
 
+    # Finally, add those IPv6 Addresses to the domain model.
     subscription.ckt.members[0].v6_ip_address = a_side_ip
     subscription.ckt.members[1].v6_ip_address = b_side_ip
 
@@ -196,7 +227,27 @@ def render_circuit_endpoint_config(
     description: str,
     address: IPv6Interface,
 ) -> str:
-    router_config = f"""! Config for {node}
+    """
+    render_circuit_endpoint_config: Renders the network device config (cisco-style)
+        for one side of a circuit
+
+    Args:
+        node (str): The node/router name (e.g. loc1-core).
+
+        interface (str): The interface name (e.g. 1/1/c2/1).
+
+        description (str): The interface description (e.g. interace-to-loc2-core:1/1/c1/1)
+
+        address (IPv6Interface): An IPv6Interface object for adding an IP to the circuit
+            interface
+
+    Returns:
+        router_config (str): The final multiline router config string that can be pasted
+            into a router on one side of a circuit.
+    """
+
+    router_config = f"""! Paste the following config into {node}:
+! to complete configuring the device
 interface {interface}
  description {description}
  ipv6 address {address}
@@ -204,7 +255,7 @@ interface {interface}
  isis network point-to-point
 !
 router isis
- net 49.0001.0123.4567.890a.0001.00
+ net {ISIS_AREA_ID}
  is-type level-2-only
  metric-style wide
  passive-interface default
@@ -216,11 +267,16 @@ router isis
 !
 end
 !"""
+
     return router_config
 
 
 @inputstep("Provide Config to User", assignee=Assignee.SYSTEM)
 def provide_config_to_user(subscription: CircuitProvisioning) -> FormGenerator:
+    """
+    Moves the Circuit to the provisioning state and then displays the config to a
+    user so that they can paste it into a router.
+    """
     logger.info(f"Creating circuit payload for Circuit #{subscription.ckt.circuit_id}")
     router_a_config = render_circuit_endpoint_config(
         node=subscription.ckt.members[0].port.node.node_name,
@@ -242,8 +298,10 @@ def provide_config_to_user(subscription: CircuitProvisioning) -> FormGenerator:
         b_side_router_config: LongText = ReadOnlyField(router_b_config)
         confirm_dry_run_results: Accept = Accept("INCOMPLETE")
 
+    logger.info("Presenting ConfigResults Form to user")
     form_data = yield ConfigResults
     user_input = form_data.dict()
+    logger.info("User confirmed router config, done with ConfigResults Form")
     return user_input
 
 
@@ -251,6 +309,9 @@ def provide_config_to_user(subscription: CircuitProvisioning) -> FormGenerator:
 def update_circuit_status_netbox(
     subscription: CircuitProvisioning,
 ) -> State:
+    """
+    Update the circuit state in netbox now that everything is done.
+    """
     circuit = netbox.dcim.cables.get(subscription.ckt.circuit_id)
     circuit.status = "connected"
     circuit.save()
@@ -263,6 +324,9 @@ def update_subscription_description(
     subscription: CircuitProvisioning,
     state: State,
 ) -> State:
+    """
+    Update the subscription description to show the final circuit information.
+    """
     subscription.description = f"Subscription for {state.get('pretty_circuit')}"
 
     return {"subscription": subscription}
