b0b33c06dffbd43b8bb5e8ac05293e9c506e7f49
chriscummings <chriscummings@es.net>
2023-04-27 14:59:59 -0500

    chore: remove user-related tests

--- tests/integration_tests/test_create_workflows.py
+++ tests/integration_tests/test_create_workflows.py
@@ -4,8 +4,6 @@ import structlog
 
 
 from tests.integration_tests.helpers import (
-    # create_user,
-    # create_user_group,
     create_node,
     create_circuit,
     get_domain_model,
@@ -13,36 +11,6 @@ from tests.integration_tests.helpers import (
 
 
 logger = structlog.get_logger(__name__)
-
-# def test_create_user_group_empty(products):
-#     group_name = "Group One"
-
-#     group_id = create_user_group(products, group_name)
-
-#     group = get_domain_model(group_id)
-
-#     assert group["description"] == f"User Group {group_name}"
-#     assert group["product"]["name"] == "User Group"
-#     assert group["user_group"]["group_name"] == group_name
-
-
-# def test_create_user_group_with_users(products):
-#     group_name = "Group Two"
-
-#     group_id = create_user_group(products, group_name)
-
-#     user_ext_id = create_user(products, group_id, "User external", "my_external_user", 30)
-#     user_int_id = create_user(products, group_id, "User internal", "my_internal_user", 40)
-
-#     user_ext = get_domain_model(user_ext_id)
-#     user_int = get_domain_model(user_int_id)
-
-#     assert user_ext["description"] == "User my_external_user from group Group Two (external)"
-#     assert user_int["description"] == "User my_internal_user from group Group Two (internal)"
-
-#     assert user_ext["user"]["group"]["group_name"] == group_name
-#     assert user_int["user"]["group"]["group_name"] == group_name
-
 
 def test_create_nodes(products):
     logger.info("Running test_create_node ")
