52cab88a2fdabef8e6c8c4514eae579da9d06e9d
chriscummings <chriscummings@es.net>
2023-04-27 14:53:19 -0500

    test: add circuit test

--- tests/integration_tests/test_create_workflows.py
+++ tests/integration_tests/test_create_workflows.py
@@ -1,14 +1,16 @@
 # pylint: disable=missing-timeout
+import re
+import structlog
+
+
 from tests.integration_tests.helpers import (
-    create_user,
-    create_user_group,
+    # create_user,
+    # create_user_group,
     create_node,
     create_circuit,
     get_domain_model,
 )
 
-
-import structlog
 
 logger = structlog.get_logger(__name__)
 
@@ -61,7 +63,59 @@ def test_create_nodes(products):
 
 
 def test_create_circuit(products):
-    pass
+    logger.info("Running test_create_circuit ")
+    circuit_names = [
+        "Circuit ID 7: loc1-core:1/1/c2/1 <--> loc2-core:1/1/c1/1",
+        "Circuit ID 8: loc1-core:1/1/c3/1 <--> loc3-core:1/1/c1/1",
+        "Circuit ID 9: loc1-core:1/1/c4/1 <--> loc4-core:1/1/c1/1",
+        "Circuit ID 10: loc1-core:1/1/c5/1 <--> loc5-core:1/1/c1/1",
+        "Circuit ID 11: loc2-core:1/1/c3/1 <--> loc3-core:1/1/c2/1",
+        "Circuit ID 12: loc2-core:1/1/c4/1 <--> loc4-core:1/1/c2/1",
+        "Circuit ID 13: loc2-core:1/1/c5/1 <--> loc5-core:1/1/c2/1",
+        "Circuit ID 14: loc3-core:1/1/c4/1 <--> loc4-core:1/1/c3/1",
+        "Circuit ID 15: loc3-core:1/1/c5/1 <--> loc5-core:1/1/c3/1",
+        "Circuit ID 16: loc4-core:1/1/c5/1 <--> loc5-core:1/1/c4/1",
+    ]
+    # node_names = ["loc5-core"]
+    for circuit in circuit_names:
+        logger.info(f'Attempting to provision Circuit "{circuit}"')
+        circuit_sub_id = create_circuit(products, circuit)
+        logger.info(
+            f'Circuit subscription "{circuit}" sucessfully run with subscription ID: {circuit_sub_id}'
+        )
+        circuit_model = get_domain_model(circuit_sub_id)
 
+        parsed_circuit = re.search(
+            r"Circuit\sID\s(\d+):\s(\S+):(\S+)\s\S+\s(\S+):(\S+)", circuit
+        )
 
+        assert circuit_model["description"] == f"Subscription for {circuit}"
+        assert circuit_model["ckt"]["circuit_id"] == int(parsed_circuit[1])
 
+        assert circuit_model["ckt"]["under_maintenance"] == True
+
+        # Test A-Side Data
+        assert (
+            circuit_model["ckt"]["members"][0]["port"]["node"]["node_name"]
+            == parsed_circuit[2]
+        )
+        assert (
+            circuit_model["ckt"]["members"][0]["port"]["port_name"] == parsed_circuit[3]
+        )
+        assert (
+            circuit_model["ckt"]["members"][0]["port"]["port_description"]
+            == f"Circuit Connection to {parsed_circuit[4]} port {parsed_circuit[5]}"
+        )
+
+        # Test B-Side Data
+        assert (
+            circuit_model["ckt"]["members"][1]["port"]["node"]["node_name"]
+            == parsed_circuit[4]
+        )
+        assert (
+            circuit_model["ckt"]["members"][1]["port"]["port_name"] == parsed_circuit[5]
+        )
+        assert (
+            circuit_model["ckt"]["members"][1]["port"]["port_description"]
+            == f"Circuit Connection to {parsed_circuit[2]} port {parsed_circuit[3]}"
+        )
